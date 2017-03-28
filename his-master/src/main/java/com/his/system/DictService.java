c443e0c143a529df9a3e2f25219a238f2f5eea8e
Ellis Zhao <zhaotieqiang@gmail.com>
2017-03-28 08:38:54 +0800

    1，机构级字典管理

--- his-master/src/main/java/com/his/system/DictService.java
+++ his-master/src/main/java/com/his/system/DictService.java
@@ -2,7 +2,10 @@ package com.his.system;
 
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
+import com.his.domain.common.entity.ErrorException;
 import com.his.domain.common.facade.BaseFacade;
+import com.his.domain.hospbase.entity.HospitalDict;
+import com.his.domain.hospbase.entity.HospitalDictType;
 import com.his.domain.system.entity.BaseDict;
 import com.his.domain.system.entity.BaseDictType;
 
@@ -38,6 +41,19 @@ public class DictService {
 
 
     /**
+     * 获取某一个医院的所有字典类型
+     * @param hospitalId
+     * @return
+     */
+    @GET
+    @Path("hosp-type-all")
+    public List<HospitalDictType> listBaseDictType(@QueryParam("hospitalId")String hospitalId){
+        String hql = "from HospitalDictType as h where h.hospitalId = '"+hospitalId+"'" ;
+        return baseFacade.createQuery(HospitalDictType.class,hql,new ArrayList<Object>()).getResultList() ;
+    }
+
+
+    /**
      * 根据传入的类型，获取字典
      * @param typeId
      * @return
@@ -47,6 +63,18 @@ public class DictService {
     public List<BaseDict> listBaseDictByType(@QueryParam("typeId") String typeId){
         String hql = "from BaseDict as d where d.typeId='"+typeId+"'" ;
         return baseFacade.createQuery(BaseDict.class,hql,new ArrayList<>()).getResultList();
+    }
+
+    /**
+     * 根据传入的类型，获取字典
+     * @param typeId
+     * @return
+     */
+    @GET
+    @Path("list-hosp-dict-by-type")
+    public List<HospitalDict> listHospDictByType(@QueryParam("typeId") String typeId){
+        String hql = "from HospitalDict as d where d.typeId='"+typeId+"'" ;
+        return baseFacade.createQuery(HospitalDict.class,hql,new ArrayList<>()).getResultList();
     }
 
 
@@ -64,6 +92,19 @@ public class DictService {
     }
 
     /**
+     * 更新键值变化
+     * @param hospitalDict
+     * @return
+     */
+    @Transactional
+    @POST
+    @Path("hosp-dict-merge")
+    public Response mergeHospitalDict(HospitalDict hospitalDict){
+        HospitalDict dict = baseFacade.merge(hospitalDict);
+        return Response.status(Response.Status.OK).entity(dict).build();
+    }
+
+    /**
      * 更新字典
      * @param baseDictType
      * @return
@@ -71,8 +112,42 @@ public class DictService {
     @Transactional
     @POST
     @Path("base-dict-type-merge")
-    public Response mergeBaseDict(BaseDictType baseDictType){
+    public Response mergeBaseDict(BaseDictType baseDictType) throws ErrorException {
+
+        String id = baseDictType.getId() ;
+        String hql = "from BaseDictType as type where upper(type.dictTypeName)=upper('"+baseDictType.getDictTypeName()+"')";
+
+        if("".equals(id)||null==id){
+            List<HospitalDictType> resultList = baseFacade.createQuery(HospitalDictType.class, hql, new ArrayList<Object>()).getResultList();
+            if(resultList.size()>0){
+                throw new ErrorException("字典已经存在，不能新增") ;
+            }
+        }
         BaseDictType dict = baseFacade.merge(baseDictType);
+        return Response.status(Response.Status.OK).entity(dict).build();
+    }
+
+
+    /**
+     * 更新字典医院字典
+     * @param hospitalDictType
+     * @return
+     */
+    @Transactional
+    @POST
+    @Path("hosp-dict-type-merge")
+    public Response mergeHospDict(HospitalDictType hospitalDictType) throws ErrorException {
+        String id = hospitalDictType.getId() ;
+        String hql = "from HospitalDictType as type where upper(type.dictTypeName)=upper('"+hospitalDictType.getDictTypeName()+"')";
+
+        if("".equals(id)||null==id){
+            List<HospitalDictType> resultList = baseFacade.createQuery(HospitalDictType.class, hql, new ArrayList<Object>()).getResultList();
+            if(resultList.size()>0){
+                throw new ErrorException("字典已经存在，不能新增") ;
+            }
+        }
+
+        HospitalDictType dict = baseFacade.merge(hospitalDictType);
         return Response.status(Response.Status.OK).entity(dict).build();
     }
 
@@ -93,6 +168,22 @@ public class DictService {
     }
 
     /**
+     * 删除医院字典
+     * @param id
+     * @return
+     */
+    @POST
+    @Path("hosp-dict-type-del")
+    @Transactional
+    public Response delHospDictType(@QueryParam("id") String id){
+        String delHql ="delete HospitalDictType where id='"+id+"'" ;
+        String delBase = "delete HospitalDict where typeId='"+id+"'" ;
+        baseFacade.executeUpdate(delHql);
+        baseFacade.executeUpdate(delBase);
+        return Response.status(Response.Status.OK).build();
+    }
+
+    /**
      * 删除键值
      * @param id
      * @return
@@ -105,6 +196,60 @@ public class DictService {
         baseFacade.executeUpdate(delBase);
         return Response.status(Response.Status.OK).build();
     }
+    /**
+     * 删除键值
+     * @param id
+     * @return
+     */
+    @POST
+    @Path("hosp-dict-del")
+    @Transactional
+    public Response delHospDict(@QueryParam("id") String id){
+        String delBase = "delete HospitalDict where id='"+id+"'" ;
+        baseFacade.executeUpdate(delBase);
+        return Response.status(Response.Status.OK).build();
+    }
 
+    /**
+     * 同步服务器字典
+     * @param hospitalId
+     * @return
+     */
+    @POST
+    @Path("syn-base")
+    @Transactional
+    public Response synHospAndSystem(@QueryParam("hospitalId")String hospitalId){
+
+
+        //删除之前的数据
+        List<HospitalDictType> preHospitalDictTypes = this.listBaseDictType(hospitalId) ;
+        for(HospitalDictType hospitalDictType :preHospitalDictTypes){
+            String hql1 = "delete HospitalDict as d where d.typeId='"+hospitalDictType.getId()+"'" ;
+            baseFacade.executeUpdate(hql1);
+            baseFacade.remove(hospitalDictType);
+        }
+
+
+        List<BaseDictType> baseDictTypes = this.listBaseDictType() ;
+        for(BaseDictType baseDictType:baseDictTypes){
+            HospitalDictType hospitalDictType = new HospitalDictType() ;
+            hospitalDictType.setHospitalId(hospitalId);
+            hospitalDictType.setDictTypeDesc(baseDictType.getDictTypeDesc());
+            hospitalDictType.setDictTypeName(baseDictType.getDictTypeName());
+
+            HospitalDictType mergeHospitalDictType = baseFacade.merge(hospitalDictType);
+
+            List<BaseDict> baseDicts =this.listBaseDictByType(baseDictType.getId()) ;
+            for(BaseDict dict:baseDicts){
+                HospitalDict hospitalDict = new HospitalDict() ;
+                hospitalDict.setDictName(dict.getDictName());
+                hospitalDict.setDictValue(dict.getDictValue());
+                hospitalDict.setInputCode(dict.getInputCode());
+                hospitalDict.setTypeId(mergeHospitalDictType.getId());
+                baseFacade.merge(hospitalDict);
+            }
+        }
+        return Response.status(Response.Status.OK).build();
+    }
 
 }
--- his-master/src/main/webapp/js/controller/hospbase/hosp-dict.js
+++ his-master/src/main/webapp/js/controller/hospbase/hosp-dict.js
@@ -2,8 +2,299 @@
  * Created by Administrator on 2017/3/23.
  */
 
-var hospDictCtrl = hisApp.controller("hospDictCtrl",function($scope,$uibModal,$http){
+var hospDictCtrl = hisApp.controller("hospDictCtrl",function($scope,$uibModal,$http,localStorageService,$state){
+
+    //判断用户是否登陆，如果用户已经登录则允许继续访问，否则跳转至登陆页面
+    $scope.loginUser=localStorageService.get("currentUser") ;
+    if(!$scope.loginUser.hospitalId){
+        parent.layer.msg("系统提示:获取登陆信息失败",{icon:2}) ;
+        $state.go("login");
+    }
 
 
 
+    $scope.hospDictData=[] ;//字典数据
+    $scope.hospTypeData=[] ;//字典类型数据
+    //字典类型列配置
+    $scope.dictTypeColumns=[{
+        headerCellClass:'headerCellClass',
+        cellClass:'cellClass',
+        field:"dictTypeName",
+        displayName:'字典名称'
+    },{
+        headerCellClass:'headerCellClass',
+        cellClass:'cellClass',
+        field:"dictTypeDesc",
+        displayName:'字典说明'
+    }] ;
+
+    //字典内容配置
+    $scope.hospDictColumns = [
+        {
+            headerCellClass:'headerCellClass',
+            cellClass:'cellClass',
+            field:'dictName',
+            displayName:'显示值'
+        },
+        {
+            headerCellClass:'headerCellClass',
+            cellClass:'cellClass',
+            field:'dictValue',
+            displayName:'实际值'
+        },
+        {
+            headerCellClass:'headerCellClass',
+            cellClass:'cellClass',
+            field:'inputCode',
+            displayName:'拼音码'
+        }
+    ] ;
+    //字典类型表格配置
+    $scope.dictTypeGridOptions={
+        enableRowSelection:true,
+        enableRowHeaderSelection:false,
+        modifierKeysToMultiSelect:false,
+        noUnselect:false,
+        enableSorting:true,
+        enableColumnMenus:false,
+        multiSelect:false,
+        columnDefs:$scope.dictTypeColumns,
+        data:$scope.hospTypeData
+    } ;
+
+    $scope.dictTypeGridOptions.onRegisterApi = function(gridApi){
+        $scope.dictTypeGridApi = gridApi ;
+        //字典类型选择事件
+        gridApi.selection.on.rowSelectionChanged($scope,function(row){
+            $scope.currentDictType = row.entity ;
+            if(row.entity.id){
+                $scope.loadDict(row.entity.id);
+            }
+        })
+    }
+
+    //字典基础表格配置
+    $scope.hospDictGridOptions = {
+        enableRowSelection:true,
+        enableRowHeaderSelection:false,
+        modifierKeysToMultiSelect:false,
+        noUnselect:false,
+        enableSorting:true,
+        enableColumnMenus:false,
+        multiSelect:false,
+        columnDefs:$scope.hospDictColumns,
+        data:$scope.hospDictData
+    } ;
+
+    //注册操作接口
+    $scope.hospDictGridOptions.onRegisterApi = function(gridApi){
+        $scope.hospDictGridApi = gridApi ;
+        //注册表格选中事件
+        gridApi.selection.on.rowSelectionChanged($scope,function(row){
+            $scope.currentHospDict = row.entity ;
+        }) ;
+    }
+    //加载所有的字典类型
+    $scope.loadDictType=function(){
+        $http.get("api/dict/hosp-type-all?hospitalId="+$scope.loginUser.hospitalId).success(function(data){
+            $scope.hospTypeData = data ;
+            $scope.dictTypeGridOptions.data = $scope.hospTypeData;
+            if($scope.hospTypeData.length){
+                $scope.dictTypeGridApi.selection.selectRow($scope.hospTypeData[0]);
+            }
+        })
+    }
+
+
+    //加载某一个字典的内容
+    $scope.loadDict=function(typeId){
+        $http.get("api/dict/list-hosp-dict-by-type?typeId="+typeId).success(function(data){
+            $scope.hospDictData = data ;
+            $scope.hospDictGridOptions.data = $scope.hospDictData ;
+        })
+    }
+
+    //加载字典
+    $scope.loadDictType();
+
+    //打开字典类型编辑窗
+    $scope.openDictTypeModal=function(action,currentTypeDict){
+        var modalInstance =$uibModal.open({
+            templateUrl:'typeModal.html',
+            controller:"typeModalInstanceCtrl",
+            controllerAs:"$ctrl",
+            backdrop:false,
+            size:"modal-sm",
+            resolve:{
+                "action":function(){
+                    return action;
+                },
+                "currentType":function(){
+                    return currentTypeDict ;
+                }
+
+            }
+
+        })
+        modalInstance.result.then(function(result){
+            if(result){
+                $scope.currentDictType = result ;
+                $scope.currentDictType.hospitalId=$scope.loginUser.hospitalId ;
+                $scope.mergeDictType() ;
+            }
+        });
+    }
+
+    //保存字典类型的变更
+    $scope.mergeDictType = function () {
+        $http.post("api/dict/hosp-dict-type-merge",$scope.currentDictType).success(function(data){
+            parent.layer.msg("系统提示：修改字典成功",{icon:1}) ;
+            $scope.loadDictType() ;
+            $scope.dictTypeGridApi.selection.selectRow($scope.currentDictType) ;
+        }) ;
+    };
+
+    $scope.openHospDictModal = function(action,hospDict){
+        var modalInstance = $uibModal.open({
+            templateUrl:"hospdict.html",
+            controller:"hospDictModalInstanceCtrl",
+            backdrop:false,
+            size:'modal-sm',
+            resolve:{
+                action:function(){
+                    return action ;
+                },
+                hospDict:function(){
+                    return hospDict ;
+                }
+            }
+        }) ;
+
+        modalInstance.result.then(function(result){
+            if(result){
+                $scope.currentHospDict = result ;
+                $scope.currentHospDict.typeId = $scope.currentDictType.id ;
+                $scope.meregeHospDict() ;
+            }
+        })
+    }
+
+    //保存字典类型的变更
+    $scope.meregeHospDict = function () {
+        $http.post("api/dict/hosp-dict-merge",$scope.currentHospDict).success(function(data){
+            parent.layer.msg("系统提示：修改字典成功",{icon:1}) ;
+            $scope.loadDict($scope.currentDictType.id);
+        }) ;
+    };
+
+    //添加字典
+    $scope.addDictType = function(){
+        $scope.currentDictType = {} ;
+        $scope.openDictTypeModal("新增",$scope.currentDictType) ;
+    }
+
+    //添加键值
+    $scope.addHospDict = function(){
+        if(!$scope.currentDictType){
+            parent.layer.msg("请选择字典类型",{icon:2}) ;
+            return ;
+        }
+
+        if(!$scope.currentDictType.id){
+            parent.layer.msg("选择字典类型尚未保存，不能够添加键值",{icon:2}) ;
+            return ;
+        }
+
+        $scope.currentHospDict ={} ;
+        $scope.openHospDictModal("新增键值",$scope.currentHospDict);
+
+    }
+
+    //删除字典
+    $scope.deleteDictType = function(){
+        if(!$scope.currentDictType){
+            parent.layer.msg("系统提示：请选择要删除的项目",{icon:2}) ;
+            return ;
+        }
+        if($scope.currentDictType.id){
+            parent.layer.confirm("删除字典，将会把字典和对应的键值数据全部删除，是否继续？",{'icon':3},function(){
+                layer.closeAll();
+                $http.post("api/dict/hosp-dict-type-del?id="+$scope.currentDictType.id).success(function(data){
+                    parent.layer.msg("系统提示：删除成功",{icon:1});
+                    $scope.loadDictType();
+                })
+            },function(){
+            })
+        }else{
+
+        }
+    }
+
+    //删除键值
+    $scope.deleteHospDict = function(){
+        if(!$scope.currentHospDict){
+            parent.layer.msg("系统提示：请选择要删除的项目",{icon:2}) ;
+            return ;
+        }
+        if($scope.currentHospDict.id){
+            parent.layer.confirm("将会把对应的键值数据，是否继续？",{'icon':3},function(){
+                layer.closeAll();
+                $http.post("api/dict/hosp-dict-del?id="+$scope.currentHospDict.id).success(function(data){
+                    parent.layer.msg("系统提示：删除成功",{icon:1});
+                    $scope.loadDict($scope.currentDictType.id);
+                })
+            },function(){
+            })
+        }else{
+
+        }
+    }
+
+    //同步服务器字典
+    $scope.synServerBaseDict = function(){
+        $http.post("api/dict/syn-base?hospitalId="+$scope.loginUser.hospitalId).success(function(){
+            $scope.loadDictType() ;
+            parent.layer.msg("系统提示：更新成功！",{"icon":1});
+        })
+    }
+
+});
+//字典窗口控制器
+var typeModalInstanceCtrl = hisApp.controller("typeModalInstanceCtrl",function(action,currentType,$uibModalInstance,$scope){
+
+    $scope.currentDictType = currentType ;
+    $scope.action = action ;
+
+    $scope.doOk= function(){
+        if(!$scope.currentDictType.id){
+            if(!$scope.currentDictType.dictTypeName){
+                parent.layer.msg("系统提示：字典名称不能为空",{"icon":2}) ;
+                return ;
+            }
+        }
+
+        $uibModalInstance.close($scope.currentDictType);
+    }
+
+    $scope.doCancel = function(){
+        $uibModalInstance.dismiss('cancel');
+    }
+})
+//键值窗口控制器
+var hospDictModalInstanceCtrl = hisApp.controller("hospDictModalInstanceCtrl",function(action,hospDict,$uibModalInstance,$scope){
+
+    $scope.currentHospDict = hospDict ;
+    $scope.action = action ;
+
+    $scope.doOk= function(){
+        if(!$scope.currentHospDict.dictName || !$scope.currentHospDict.dictValue){
+            parent.layer.msg("系统提示：键值不能为空",{"icon":2}) ;
+            return ;
+        }
+        $uibModalInstance.close($scope.currentHospDict);
+    }
+
+    $scope.doCancel = function(){
+        $uibModalInstance.dismiss('cancel');
+    }
 })
